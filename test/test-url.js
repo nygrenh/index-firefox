3ec966abc2fd8a6ee13f179b217ae624257d9076
Henrik Nygren <nygrenh@gmail.com>
2014-05-27 22:49:44 +0300

    Added some unit tests for url.js

---
+++ test/test-url.js
@@ -0,0 +1,14 @@
+var url = require("./url.js");
+var prefs = require("sdk/simple-prefs").prefs;
+
+exports["test url starts with the Index url"] = function(assert){
+  prefs.url = "https://a.com/";
+  assert.ok(url.construct().indexOf("https://a.com/") === 0, "starts with url");
+};
+
+exports["test url is correct without trailing slash in the Index url"] = function(assert){
+  prefs.url = "https://a.com";
+  assert.ok(url.construct().indexOf("https://a.com/") === 0, "url with trailing slash");
+};
+
+require("sdk/test").run(exports);
